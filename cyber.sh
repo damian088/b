25ed2e356b842f4a88235a9c59407686ce2a1f42
Mya Cobb <111387413+myacobb@users.noreply.github.com>
2023-12-21 14:48:53 -0600

    Add files via upload

---
+++ cyber.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Metasploit
+git clone https://github.com/rapid7/metasploit-framework.git
+
+# Wireshark
+apt install wireshark
+
+# Nmap
+apt install nmap
+
+# Hydra
+apt install hydra
+
+# Beef
+apt install beef-xss
+
+# SQL map
+apt install sqlmap
+
+# Aircrack
+apt install aircrack-ng
+
+# Bettercap
+apt install bettercap
+
+# Burpsuite
+apt install burpsuite
+
+# Cupp for wordlists
+git clone https://github.com/Mebus/cupp.git
+
+# Hping3
+apt install hping3
+
+# Sherlock
+apt install sherlock
+
+# Arp scan
+apt install arp-scan
+
+# Medusa
+apt install medusa
+
